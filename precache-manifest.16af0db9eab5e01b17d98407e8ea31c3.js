30923ed618b88520f2abff31f27649ea841af33d
jaygala24 <jaygala24@gmail.com>
2018-11-21 13:31:40 +0530

    Updates

---
+++ precache-manifest.16af0db9eab5e01b17d98407e8ea31c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "07974b2a5c682207c6ea",
+    "url": "/contactmanager/static/js/main.07974b2a.chunk.js"
+  },
+  {
+    "revision": "3c350c1e4b4432cf2899",
+    "url": "/contactmanager/static/js/1.3c350c1e.chunk.js"
+  },
+  {
+    "revision": "07974b2a5c682207c6ea",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "3c350c1e4b4432cf2899",
+    "url": "/contactmanager/static/css/1.166ee869.chunk.css"
+  },
+  {
+    "revision": "68d7cdb695d04b29152024163ff5f384",
+    "url": "/contactmanager/index.html"
+  }
+];
